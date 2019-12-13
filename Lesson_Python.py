9d621dcfb0f50345fd22d3d30ae5aecdbca7a6d3
Anatolyvgy <anatolyitstep@gmail.com>
2019-12-14 02:30:51 +0300

    second way

--- Lesson_Python.py
+++ Lesson_Python.py
@@ -1,3 +1,3 @@
 rubles = int(input("Таки сколько вы имеете рублей?\n"))
 kop = int(input("А копеек?\n"))
-print("У меня есть", rubles ,"рублей", kop,"копеек")
+print("У меня есть %s рублей %s копеек"%(rubles, kop))
