a2994ab098a1efaa90653afc6337e499c5f7b198
Anatolyvgy <anatolyitstep@gmail.com>
2019-12-15 00:48:18 +0300

    division

--- Lesson_Python.py
+++ Lesson_Python.py
@@ -1,3 +1,7 @@
-rubles = int(input("Таки сколько вы имеете рублей?\n"))
-kop = int(input("А копеек?\n"))
-print("У меня есть %s рублей %s копеек"%(rubles, kop))
+x=int(input())
+a=x//10000
+b=x//1000%10
+c=x//100%10
+d=x//10%10
+e=x%10
+print(a,b,c,d,e,sep='\n')
